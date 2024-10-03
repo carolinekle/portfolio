1fa2999ecab13284096d9b282573e949e660d3e2
carolinekle <caroline.klewinowski@gmail.com>
2024-10-03 17:58:39 -0400

    start writing components

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <>
+
+      <div className="nav">
+
+            <h1>
+              <Link to="/">caroline</Link>
+            </h1>
+            <h1>
+              <Link to="projects">projects</Link>
+            </h1>
+            <h1>
+              <Link to="contact">contact</Link>
+            </h1>
+
+            <div className = "fun">
+            
+            </div>
+      </div>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
---
+++ src/pages/projects.js
@@ -0,0 +1,23 @@
+import Nav from "../components/Nav";
+
+export default function Contact() {
+    return (
+
+        
+      <main>
+      <section className="footer-section">
+        <Nav />
+      </section>
+        <section className="content">
+          <section className="welcome-section">
+            <p>Contact</p>
+            <h1> React blogs Home page</h1>
+            <p>
+              click the link on the navbar to go to a different section on the
+              page
+            </p>
+          </section>
+        </section>
+      </main>
+    );
+  }
